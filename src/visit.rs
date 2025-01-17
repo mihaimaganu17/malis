ffb5e3e80f56d53d8f255e4fb8b8615ec00a4f56
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-01-17 07:56:26 +0100

    Visitor pattern initial scheme

--- src/visit.rs
+++ src/visit.rs
@@ -1,9 +1,18 @@
 use crate::ast::{Expr, Unary, Binary, Literal, Grouping};
 
 pub trait Visitor<T> {
-    fn visit_expr(&mut self, expr: &Expr) -> T;
     fn visit_unary(&mut self, unary: &Unary) -> T;
     fn visit_binary(&mut self, binary: &Binary) -> T;
     fn visit_literal(&mut self, literal: &Literal) -> T;
     fn visit_grouping(&mut self, grouping: &Grouping) -> T;
 }
+
+#[derive(Debug)]
+pub struct AstPrinter;
+
+impl Visitor<String> for AstPrinter {
+    fn visit_binary(&mut self, unary: &Unary) -> String {
+    }
+}
+
+pub fn walk_expr<E: Expr>(visitor: &mut Visitor, e: &Expr)
