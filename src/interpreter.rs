ad6f01ecac8ed72872a1b30017eb3d445232d449
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-01-23 10:22:58 +0100

    Added Unary expression evaluation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,5 @@
 use crate::visit::Visitor;
+use crate::token::{TokenType, SingleChar};
 use crate::ast::{Unary, Binary, Ternary, Literal, LiteralType, Group};
 
 #[derive(Debug)]
@@ -14,9 +15,35 @@ pub enum MalisObject {
 pub struct Interpreter {}
 
 impl Visitor<MalisObject> for Interpreter {
-    fn visit_unary(&mut self, _unary: &Unary) -> MalisObject {
-        return MalisObject::Nil;
+    fn visit_unary(&mut self, unary: &Unary) -> MalisObject {
+        // We first evaluate the operand expression;
+        let right_malis_object = unary.right.walk(self);
+        // Our interpreter is doing a post-order traversal - each node evaluates its children
+        // before doing its own work. As such we first evaluated the underlying expression above
+        // and now we are evaluating the operator of our current value
+        if let Some(operator_type) = unary.operator.t_type.get() {
+            match operator_type {
+                TokenType::SingleChar(SingleChar::Minus) => {
+                    if let MalisObject::Number(n) = right_malis_object {
+                        MalisObject::Number(-n)
+                    } else {
+                        panic!("Cannot minus object {:?} at {:?}", right_malis_object, unary.operator.line);
+                    }
+                }
+                TokenType::SingleChar(SingleChar::Bang) => {
+                    match right_malis_object {
+                        MalisObject::True => MalisObject::False,
+                        MalisObject::False => MalisObject::True,
+                        _ => panic!("Cannot negate object {:?} at {:?}", right_malis_object, unary.operator.line),
+                    }
+                }
+                _ => panic!("Invalid unary operator {:?}", unary.operator),
+            }
+        } else {
+            panic!("Unary operator {:?} has not TokenType {:?}", unary.operator.lexeme, unary.operator.line);
+        }
     }
+
     fn visit_binary(&mut self, _binary: &Binary) -> MalisObject {
         return MalisObject::Nil;
     }
