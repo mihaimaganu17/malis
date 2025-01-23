85fca1ecd43edbc0831b180cc7c3689f8517ffd2
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-01-23 10:00:16 +0100

    Added literal evaluation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,11 +1,13 @@
 use crate::visit::Visitor;
-use crate::ast::{Unary, Binary, Ternary, Literal, Group};
+use crate::ast::{Unary, Binary, Ternary, Literal, LiteralType, Group};
 
 #[derive(Debug)]
 pub enum MalisObject {
     Boolean(bool),
     Number(f32),
-    MalisString(String),
+    StringValue(String),
+    True,
+    False,
     Nil,
 }
 
@@ -21,8 +23,19 @@ impl Visitor<MalisObject> for Interpreter {
     fn visit_ternary(&mut self, _ternary: &Ternary) -> MalisObject {
         return MalisObject::Nil;
     }
-    fn visit_literal(&mut self, _literal: &Literal) -> MalisObject {
-        return MalisObject::Nil;
+    // Evaluating literals. A literal is a `bit of syntax` that produces a vlue. A literal
+    // always appears somewhere in the user's source code. Lots of values are produces by
+    // computation and don't exist anywhere in the code itself, but those are not literals.
+    // A literal comes from the parser's domain. Values are an interpreter concept, part of the
+    // runtime's world.
+    fn visit_literal(&mut self, literal: &Literal) -> MalisObject {
+        match &literal.l_type {
+            LiteralType::Number(n) => MalisObject::Number(*n),
+            LiteralType::LitString(s) => MalisObject::StringValue(s.to_string()),
+            LiteralType::True => MalisObject::True,
+            LiteralType::False => MalisObject::False,
+            LiteralType::Nil => MalisObject::Nil,
+        }
     }
     fn visit_group(&mut self, _group: &Group) -> MalisObject {
         return MalisObject::Nil;
