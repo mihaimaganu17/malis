0989cffb66a11e43f8f8ab04fd7b2813f850df88
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-02-17 09:54:34 +0100

    Refactore MalisObject and Funciton types in their own modules

---
+++ src/interpreter/function.rs
@@ -0,0 +1,159 @@
+use super::{Interpreter, MalisObject, RuntimeError};
+use core::cmp::Ordering;
+use std::fmt;
+use crate::{token::Token, ast::FunctionDeclaration};
+
+pub trait MalisCallable {
+    fn arity(&self) -> Result<usize, RuntimeError>;
+
+    fn call(
+        &self,
+        interpreter: &mut Interpreter,
+        arguments: Vec<MalisObject>,
+    ) -> Result<MalisObject, RuntimeError>;
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub struct NativeFunction {
+    name: String,
+    arity: usize,
+    call_fn: fn(&mut Interpreter, Vec<MalisObject>) -> Result<MalisObject, RuntimeError>,
+}
+
+impl NativeFunction {
+    pub fn new(
+        name: String,
+        arity: usize,
+        call_fn: fn(&mut Interpreter, Vec<MalisObject>) -> Result<MalisObject, RuntimeError>,
+    ) -> Self {
+        Self {
+            name,
+            arity,
+            call_fn,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+}
+
+impl MalisCallable for NativeFunction {
+    fn arity(&self) -> Result<usize, RuntimeError> {
+        Ok(self.arity)
+    }
+
+    fn call(
+        &self,
+        interpreter: &mut Interpreter,
+        arguments: Vec<MalisObject>,
+    ) -> Result<MalisObject, RuntimeError> {
+        (self.call_fn)(interpreter, arguments)
+    }
+}
+
+impl MalisCallable for Box<NativeFunction> {
+    fn arity(&self) -> Result<usize, RuntimeError> {
+        Ok(self.arity)
+    }
+
+    fn call(
+        &self,
+        interpreter: &mut Interpreter,
+        arguments: Vec<MalisObject>,
+    ) -> Result<MalisObject, RuntimeError> {
+        (self.call_fn)(interpreter, arguments)
+    }
+}
+
+#[derive(Clone)]
+pub struct UserFunction {
+    function_declaration: FunctionDeclaration,
+}
+
+impl UserFunction {
+    pub fn new(function_declaration: FunctionDeclaration) -> Self {
+        UserFunction {
+            function_declaration,
+        }
+    }
+
+    pub fn name(&self) -> &Token {
+        &self.function_declaration.name
+    }
+}
+
+impl MalisCallable for UserFunction {
+    fn arity(&self) -> Result<usize, RuntimeError> {
+        Ok(self.function_declaration.parameters.len())
+    }
+
+    fn call(
+        &self,
+        interpreter: &mut Interpreter,
+        arguments: Vec<MalisObject>,
+    ) -> Result<MalisObject, RuntimeError> {
+        // Create a new environment that encapsulates the parameters
+        let mut environment = interpreter.environment.take();
+        // Define all the parameters of the function in the new environment
+        for (param, arg) in self
+            .function_declaration
+            .parameters
+            .iter()
+            .zip(arguments.into_iter())
+        {
+            environment.define(param.lexeme().to_string(), arg)?;
+        }
+
+        // With the new environment defined, execute the body of the function
+        interpreter.execute_block(&self.function_declaration.body, environment)?;
+
+        Ok(MalisObject::Nil)
+    }
+}
+
+impl fmt::Debug for UserFunction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "<fn {}> (", self.function_declaration.name)?;
+
+        for param in &self.function_declaration.parameters {
+            write!(f, "{},", param)?;
+        }
+
+        writeln!(f, ")")
+    }
+}
+
+impl PartialEq for UserFunction {
+    fn eq(&self, other: &Self) -> bool {
+        if self.function_declaration.name != other.function_declaration.name {
+            return false;
+        }
+
+        if self.function_declaration.parameters.len() != other.function_declaration.parameters.len()
+        {
+            return false;
+        }
+
+        for (params, other_params) in self
+            .function_declaration
+            .parameters
+            .iter()
+            .zip(other.function_declaration.parameters.iter())
+        {
+            if params != other_params {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+impl PartialOrd for UserFunction {
+    fn partial_cmp(&self, _other: &Self) -> Option<Ordering> {
+        // We do not order functions, but we need to implement this trait in order to confirm to
+        // the `MalisObject` protocol
+        None
+    }
+}
---
+++ src/interpreter/object.rs
@@ -0,0 +1,217 @@
+use core::ops::{Add, Div, Mul, Neg, Not, Sub};
+use std::fmt;
+use super::{Interpreter, MalisCallable, NativeFunction, UserFunction, RuntimeError};
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub enum MalisObject {
+    Boolean(bool),
+    Number(f32),
+    StringValue(String),
+    NativeFunction(Box<NativeFunction>),
+    UserFunction(UserFunction),
+    Nil,
+}
+
+impl fmt::Display for MalisObject {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        match self {
+            Self::Boolean(value) => write!(f, "{value}"),
+            Self::StringValue(value) => write!(f, "{value}"),
+            Self::Nil => write!(f, "nil"),
+            Self::Number(value) => write!(f, "{}", value),
+            Self::NativeFunction(value) => write!(f, "<native fn {}>", value.name()),
+            Self::UserFunction(value) => write!(f, "<fn {}>", value.name()),
+        }
+    }
+}
+
+impl MalisObject {
+    // Decides whether a `MalisObject` value is true or not inside the context of the `Malis`
+    // language
+    pub fn is_truthy(&self) -> bool {
+        match self {
+            MalisObject::Boolean(b) => *b,
+            // We consider any value coming from a literal as true. What do we do about
+            // 0?
+            MalisObject::StringValue(_) | MalisObject::Number(_) => true,
+            // We consider function pointers as true
+            MalisObject::NativeFunction(_) | MalisObject::UserFunction(_) => true,
+            // We consider null as false
+            MalisObject::Nil => false,
+        }
+    }
+
+    pub fn is_callable(&self) -> bool {
+        matches!(self, MalisObject::NativeFunction(_))
+            || matches!(self, MalisObject::UserFunction(_))
+    }
+}
+
+impl MalisCallable for MalisObject {
+    fn arity(&self) -> Result<usize, RuntimeError> {
+        match self {
+            MalisObject::NativeFunction(f) => f.arity(),
+            MalisObject::UserFunction(f) => f.arity(),
+            _ => Err(RuntimeError::NotCallable(format!(
+                "Object {} has no arity.",
+                self
+            ))),
+        }
+    }
+
+    fn call(
+        &self,
+        interpreter: &mut Interpreter,
+        arguments: Vec<MalisObject>,
+    ) -> Result<MalisObject, RuntimeError> {
+        match self {
+            MalisObject::NativeFunction(f) => f.call(interpreter, arguments),
+            MalisObject::UserFunction(f) => f.call(interpreter, arguments),
+            _ => Err(RuntimeError::NotCallable(format!(
+                "Object {} is not callable.",
+                self
+            ))),
+        }
+    }
+}
+
+impl From<bool> for MalisObject {
+    fn from(v: bool) -> Self {
+        Self::Boolean(v)
+    }
+}
+
+impl Not for MalisObject {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        if self.is_truthy() {
+            MalisObject::Boolean(true)
+        } else {
+            MalisObject::Boolean(false)
+        }
+    }
+}
+
+impl Neg for MalisObject {
+    type Output = Result<Self, RuntimeError>;
+
+    fn neg(self) -> Self::Output {
+        if let MalisObject::Number(n) = self {
+            Ok(MalisObject::Number(-n))
+        } else {
+            Err(RuntimeError::Negation(format!(
+                "Cannot negate object {:?}",
+                self
+            )))
+        }
+    }
+}
+
+impl Add for MalisObject {
+    type Output = Result<Self, RuntimeError>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match self {
+            MalisObject::Number(left) => match rhs {
+                MalisObject::Number(right) => Ok(MalisObject::Number(left + right)),
+                MalisObject::StringValue(right) => {
+                    Ok(MalisObject::StringValue(format!("{left}{right}")))
+                }
+                _ => Err(RuntimeError::Addition(format!(
+                    "Cannot add objects {:?} and {:?}",
+                    self, rhs
+                ))),
+            },
+            MalisObject::StringValue(ref left) => match rhs {
+                MalisObject::StringValue(right) => {
+                    Ok(MalisObject::StringValue(format!("{left}{right}")))
+                }
+                MalisObject::Number(right) => {
+                    Ok(MalisObject::StringValue(format!("{left}{right}")))
+                }
+                _ => Err(RuntimeError::Addition(format!(
+                    "Cannot add objects {:?} and {:?}",
+                    self, rhs
+                ))),
+            },
+            _ => Err(RuntimeError::Addition(format!(
+                "Cannot add objects {:?} and {:?}",
+                self, rhs
+            ))),
+        }
+    }
+}
+
+impl Sub for MalisObject {
+    type Output = Result<Self, RuntimeError>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if let MalisObject::Number(left) = self {
+            if let MalisObject::Number(right) = rhs {
+                Ok(MalisObject::Number(left - right))
+            } else {
+                Err(RuntimeError::Subtraction(format!(
+                    "Cannot subtract objects {:?} and {:?}",
+                    self, rhs
+                )))
+            }
+        } else {
+            Err(RuntimeError::Subtraction(format!(
+                "Cannot subtract objects {:?} and {:?}",
+                self, rhs
+            )))
+        }
+    }
+}
+
+impl Mul for MalisObject {
+    type Output = Result<Self, RuntimeError>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if let MalisObject::Number(left) = self {
+            if let MalisObject::Number(right) = rhs {
+                Ok(MalisObject::Number(left * right))
+            } else {
+                Err(RuntimeError::Multiplication(format!(
+                    "Cannot multiply objects {:?} and {:?}",
+                    self, rhs
+                )))
+            }
+        } else {
+            Err(RuntimeError::Multiplication(format!(
+                "Cannot multiply objects {:?} and {:?}",
+                self, rhs
+            )))
+        }
+    }
+}
+
+impl Div for MalisObject {
+    type Output = Result<Self, RuntimeError>;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        if let MalisObject::Number(left) = self {
+            if let MalisObject::Number(right) = rhs {
+                if right == 0.0 {
+                    Err(RuntimeError::Division(format!(
+                        "Zero is an invalid denominator {:?}!",
+                        right
+                    )))
+                } else {
+                    Ok(MalisObject::Number(left / right))
+                }
+            } else {
+                Err(RuntimeError::Division(format!(
+                    "Cannot divide objects {:?} and {:?}",
+                    self, rhs
+                )))
+            }
+        } else {
+            Err(RuntimeError::Division(format!(
+                "Cannot divide objects {:?} and {:?}",
+                self, rhs
+            )))
+        }
+    }
+}
